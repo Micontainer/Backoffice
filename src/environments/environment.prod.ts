cfda76ca28a898da6a410e2f0ab2c66ac547e654
FedeMFernandez <fedefernandez218@gmail.com>
2023-07-12 13:07:39 -0300

    Added env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiURL: 'https://yb4we67wll.execute-api.us-east-1.amazonaws.com/qa/',
-  apiKey: 'd41d8cd98f00b204e9800998ecf8427e',
+  apiURL: 'https://yb4we67wll.execute-api.us-east-1.amazonaws.com/qa/api/v1/',
+  apiKey: 'rxcJRl6Oo42OHSbfTG30p6weYjWVfBa24MGLZfZq',
 };
