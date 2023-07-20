25b5957d7ddf94e550c496e607a3f2a4af87e33b
FedeMFernandez <fedefernandez218@gmail.com>
2023-07-20 17:03:21 -0300

    router url changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiURL: 'https://yb4we67wll.execute-api.us-east-1.amazonaws.com/qa/api/v1/',
+  apiURL: 'https://yb4we67wll.execute-api.us-east-1.amazonaws.com/qa/',
   apiKey: 'rxcJRl6Oo42OHSbfTG30p6weYjWVfBa24MGLZfZq',
 };
