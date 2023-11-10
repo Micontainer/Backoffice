bf65433ebd668951dceb72940e2913bfc5e13a9c
FedeMFernandez <fedefernandez218@gmail.com>
2023-11-10 14:50:03 -0300

    showing token

--- src/app/commons/services/push.service.ts
+++ src/app/commons/services/push.service.ts
@@ -36,6 +36,8 @@ export class PushService extends BaseService {
             console.log('No registration token available. Request permission to generate one.');
           }
 
+          console.log(token);
+
           await this.subscribe({ token });
         }).catch((err) => {
           console.log('An error occurred while retrieving token. ', err);
